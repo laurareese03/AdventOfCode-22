55ed58f09572fc6903e04a83437165796b3b9020
Laura Reese <laurareese03@gmail.com>
2022-12-08 16:42:14 -0500

    Day 8 part b solution, and some style fixes

--- day8/treetop-house.py
+++ day8/treetop-house.py
@@ -11,21 +11,52 @@ trees = np.array(arr)
 visible = np.zeros(trees.shape)
 
 def get_lr_columns():
-  # idea - search from left to right, then transpose matrix and search from left to right again.
   for row in range(0, len(trees)):
     for column in range(0, len(trees[row])):
       if (all(x < trees[row][column] for x in trees[row][:column])):
         visible[row][column] = 1
 
-get_lr_columns()
-trees = np.fliplr(trees)
-visible = np.fliplr(visible)
-get_lr_columns()
-trees = trees.transpose()
-visible = visible.transpose()
-get_lr_columns()
-trees = np.fliplr(trees)
-visible = np.fliplr(visible)
-get_lr_columns()
+def part_a():
+  global trees
+  global visible
+  get_lr_columns()
+  trees = np.fliplr(trees)
+  visible = np.fliplr(visible)
+  get_lr_columns()
+  trees = trees.transpose()
+  visible = visible.transpose()
+  get_lr_columns()
+  trees = np.fliplr(trees)
+  visible = np.fliplr(visible)
+  get_lr_columns()
 
-print(np.sum(visible))
+  print(np.sum(visible))
+
+def get_tree_distance(array, height):
+  for i in range(0, len(array)):
+    if not (all(x < height for x in array[:i])):
+      return i
+  return len(array)
+
+def get_tree_score(row, column):
+  global trees
+  height = trees[row, column]
+  score = (
+    get_tree_distance(np.flipud(trees[row][:column]), height) *
+    get_tree_distance(trees[row][column+1:], height) *
+    get_tree_distance(trees[row+1:,column], height) *
+    get_tree_distance(np.flipud(trees[:row,column]), height) 
+  )
+  return score
+
+def part_b():
+  maxTreeScore = 0
+  for row in range(0, len(trees)):
+    for column in range(0, len(trees[row])):
+      treeScore = get_tree_score(row, column)
+      maxTreeScore = max(maxTreeScore, treeScore)
+
+  print(maxTreeScore)
+
+part_a()
+part_b()
