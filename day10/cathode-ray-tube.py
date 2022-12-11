b75a98c5b169c11b8cf4c865a585f437cfe11706
Laura Reese <laurareese03@gmail.com>
2022-12-10 21:02:23 -0500

    day 10 part b solution

--- day10/cathode-ray-tube.py
+++ day10/cathode-ray-tube.py
@@ -3,7 +3,8 @@ with open('program.txt') as f:
 
 count = 1
 cycle = 1
-signalSum = 0
+crtRow = ''
+crtRows = []
 
 def get_signal_strength(count, cycle):
   return count * cycle
@@ -12,18 +13,39 @@ for line in lines:
   line = line.strip()
 
   if line == 'noop':
-    if ((cycle + 20) % 40 == 0 and cycle < 230):
-      signalSum += get_signal_strength(count, cycle)
+    if ((cycle-1) % 40 == 0):
+      crtRows.append(crtRow)
+      crtRow = ''
+    if (count == len(crtRow) or count+1 == len(crtRow) or count-1 == len(crtRow)):
+      crtRow += '#'
+    else:
+      crtRow += '.'
     cycle += 1
   
   else:
     line = line.split(' ')
-    if ((cycle + 20) % 40 == 0 and cycle < 230):
-      signalSum += get_signal_strength(count, cycle)
+    if ((cycle-1) % 40 == 0):
+      crtRows.append(crtRow)
+      crtRow = ''
+    if (count == len(crtRow) or count+1 == len(crtRow) or count-1 == len(crtRow)):
+      crtRow += '#'
+    else:
+      crtRow += '.'
     cycle += 1
-    if ((cycle + 20) % 40 == 0 and cycle < 230):
-      signalSum += get_signal_strength(count, cycle)
+    if ((cycle-1) % 40 == 0):
+      crtRows.append(crtRow)
+      crtRow = ''
+
+    if (count == len(crtRow) or count+1 == len(crtRow) or count-1 == len(crtRow)):
+      crtRow += '#'
+    else:
+      crtRow += '.'
     cycle += 1
     count += int(line[1])
 
-print(signalSum)
+  print(crtRow)
+
+crtRows.append(crtRow)
+
+for row in crtRows:
+  print(row)
