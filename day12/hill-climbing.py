213c61690a575006c8c531b600175aea34d5dcbc
Laura Reese <laurareese03@gmail.com>
2022-12-12 17:13:50 -0500

    day 12 solution B? more like solution ugly

--- day12/hill-climbing.py
+++ day12/hill-climbing.py
@@ -10,34 +10,48 @@ for geo in hill:
 hill = np.array(arr)
 start = np.where(hill == 'S')
 start = (start[0][0], start[1][0])
+hill[start] = 'a'
 end = np.where(hill == 'E')
 end = (end[0][0], end[1][0])
 hill[end] = 'z'
 
-# Breadth First Search baybee
-frontier = [[start, 0]]
-frontierButWorse = [start]
-visited = []
-height = ord('a')
+minDist = 1000
 
-while len(frontier) != 0:
-  place = frontier.pop()
-  if place[0] == end:
-    print(place)
-    break
-  if hill[tuple(place[0])] != 'S':
-    height = ord(hill[place[0]])
-  visited.append(place[0])
-  
-  if place[0][0] + 1 < len(hill) and ord(hill[place[0][0]+1, place[0][1]]) <= (height + 1) and (place[0][0]+1, place[0][1]) not in visited and (place[0][0]+1, place[0][1]) not in frontierButWorse:
-    frontier.insert(0, [(place[0][0]+1, place[0][1]), place[1]+1])
-    frontierButWorse.append((place[0][0]+1, place[0][1]))
-  if place[0][1] + 1 < len(hill[place[0][0]]) and ord(hill[place[0][0], place[0][1]+1]) <= (height + 1) and (place[0][0], place[0][1]+1) not in visited and (place[0][0], place[0][1]+1) not in frontierButWorse:
-    frontier.insert(0, [(place[0][0], place[0][1]+1), place[1]+1])
-    frontierButWorse.append((place[0][0], place[0][1]+1))
-  if place[0][0] - 1 >= 0 and ord(hill[place[0][0]-1, place[0][1]]) <= (height + 1) and (place[0][0]-1, place[0][1]) not in visited and (place[0][0]-1, place[0][1]) not in frontierButWorse:
-    frontier.insert(0, [(place[0][0]-1, place[0][1]), place[1]+1])
-    frontierButWorse.append((place[0][0]-1, place[0][1]))
-  if place[0][1] - 1 >= 0 and ord(hill[place[0][0], place[0][1]-1]) <= (height + 1) and (place[0][0], place[0][1]-1) not in visited and (place[0][0], place[0][1]-1) not in frontierButWorse:
-    frontier.insert(0, [(place[0][0], place[0][1]-1), place[1]+1])
-    frontierButWorse.append((place[0][0], place[0][1]-1))
+for startingPoint in range(0, len(hill)):
+  start = (startingPoint, 0)
+
+
+  # Breadth First Search baybee
+  frontier = [[start, 0]]
+  frontierButWorse = [start]
+  visited = []
+  height = ord('a')
+
+  while len(frontier) != 0:
+    place = frontier.pop()
+    if place[0] == end:
+      minDist = place[1]
+      print(place[1])
+      break
+
+    if place[1] > minDist:
+      break
+
+    if hill[tuple(place[0])] != 'S':
+      height = ord(hill[place[0]])
+    visited.append(place[0])
+    
+    if place[0][0] + 1 < len(hill) and ord(hill[place[0][0]+1, place[0][1]]) <= (height + 1) and (place[0][0]+1, place[0][1]) not in visited and (place[0][0]+1, place[0][1]) not in frontierButWorse:
+      frontier.insert(0, [(place[0][0]+1, place[0][1]), place[1]+1])
+      frontierButWorse.append((place[0][0]+1, place[0][1]))
+    if place[0][1] + 1 < len(hill[place[0][0]]) and ord(hill[place[0][0], place[0][1]+1]) <= (height + 1) and (place[0][0], place[0][1]+1) not in visited and (place[0][0], place[0][1]+1) not in frontierButWorse:
+      frontier.insert(0, [(place[0][0], place[0][1]+1), place[1]+1])
+      frontierButWorse.append((place[0][0], place[0][1]+1))
+    if place[0][0] - 1 >= 0 and ord(hill[place[0][0]-1, place[0][1]]) <= (height + 1) and (place[0][0]-1, place[0][1]) not in visited and (place[0][0]-1, place[0][1]) not in frontierButWorse:
+      frontier.insert(0, [(place[0][0]-1, place[0][1]), place[1]+1])
+      frontierButWorse.append((place[0][0]-1, place[0][1]))
+    if place[0][1] - 1 >= 0 and ord(hill[place[0][0], place[0][1]-1]) <= (height + 1) and (place[0][0], place[0][1]-1) not in visited and (place[0][0], place[0][1]-1) not in frontierButWorse:
+      frontier.insert(0, [(place[0][0], place[0][1]-1), place[1]+1])
+      frontierButWorse.append((place[0][0], place[0][1]-1))
+
+print(minDist)
