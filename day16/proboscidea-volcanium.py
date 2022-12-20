3dacea6ff268e8ce0222c90e35551a0271e4a395
Laura Reese <laurareese03@gmail.com>
2022-12-20 13:15:11 -0600

    Day 16 part a solution

--- day16/proboscidea-volcanium.py
+++ day16/proboscidea-volcanium.py
@@ -1,4 +1,4 @@
-import re
+import re, time as t
 
 with open('valves.txt') as f:
   lines = f.readlines()
@@ -18,33 +18,37 @@ for line in lines:
 
 # pressure tracking and decision making
 builtPressure = 0 # for opened valves
-releasedPressure = 0 # will track pressure over time
+# will track pressure over time
+maxv = 0
+def get_path(start, time, releasedPressure):
+  global maxv
+  print("START", releasedPressure, start, time)
+  if time <= 0:
+    return releasedPressure
+  frontier = [[tunnels[start], 0, 0]]
+  visited = {}
+  while len(frontier) != 0:
+    valve = frontier.pop(0)
+    for connection in valve[0]['connections']:
+      if connection not in visited.keys():
+        frontier.append([tunnels[connection], valve[1]+1])
+        visited[connection] = ((time-(valve[1]+2))*tunnels[connection]['pressure'])
 
-arr = []
-frontier = [[tunnels['AA'], 0, 0]]
-visited = {}
-while len(frontier) != 0:
-  valve = frontier.pop(0)
-  for connection in valve[0]['connections']:
-    if connection not in visited.keys():
-      frontier.append([tunnels[connection], valve[1]+1])
-      visited[connection] = ((30-(valve[1]+1))*tunnels[connection]['pressure'])
+  visited = {k: v for k, v in sorted(visited.items(), key=lambda item: item[1], reverse=True)}
+  for visit in visited.keys(): 
+    if visited[visit] == 0:
+      return releasedPressure
+    if not tunnels[visit]['isOpen']:
+      tunnels[visit]['isOpen'] = True
+      timeDist = time - (visited[visit]/tunnels[visit]['pressure'])
+      value = get_path(visit, time - int(timeDist), releasedPressure + visited[visit])
+      maxv = max(maxv, int(value or 0))
+      tunnels[visit]['isOpen'] = False
 
-print(visited)
-
-
-
+get_path('AA', 30, 0)
+print("HERE", maxv)
 
 # def get_next_move():
   # 2 kinds of return values
   # if next decision is move, return 0
   # else next decision is open current valve, return pressure of valve
-
-
-
-
-# time simulation
-for i in range(0, 30): # time limit
-  releasedPressure += builtPressure
-# find_best_route('AA')
-  # builtPressure = get_next_move()
