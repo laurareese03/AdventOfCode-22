1f05bb3fae44fd635b41a0295840ecd59af5664e
Laura Reese <laurareese03@gmail.com>
2022-12-22 02:24:05 -0600

    Day 17 part a and brute force attempt at part b

--- day17/pyroclastic-flow.py
+++ day17/pyroclastic-flow.py
@@ -1,6 +1,6 @@
 with open('wind-gusts.txt') as f:
   lines = f.readlines()
-  wind = lines[0]
+  wind = lines[0].strip()
 
 shapes = [['####'], ['.#.','###','.#.'], ['###','..#','..#'], ['#','#','#','#'], ['##','##']]
 arena = ['#######']
@@ -8,24 +8,17 @@ windIndex = 0
 
 def get_clashing_pieces(direction, leftIndex, shape, arenaLine):
   clashing = False
-  for k in range(0, len(shape)):
-    try:
-      arenaHash = [n for n in range(len(arena[arenaLine + k])) if arena[arenaLine+k].find('#', n) == n]
-      shapeHash = [n for n in range(len(shape[k])) if shape[k].find('#', n) == n]
-      if direction == '>':
-        clashing = clashing or leftIndex + shapeHash[-1] + 1 in arenaHash # right
-      else:
-        clashing = clashing or leftIndex - shapeHash[0] - 1 in arenaHash # left
-    except:
-      print('blah')
+  for k in range(len(shape)):
+    arenaHash = [n for n in range(len(arena[arenaLine + k])) if arena[arenaLine+k].find('#', n) == n]
+    shapeHashPlus = [(n+leftIndex+1) for n in range(len(shape[k])) if shape[k].find('#', n) == n]
+    shapeHashMinus = [(n+leftIndex-1) for n in range(len(shape[k])) if shape[k].find('#', n) == n]
+    if direction == '>':
+      clashing = clashing or len(set(shapeHashPlus).intersection(set(arenaHash))) != 0 # right
+    else:
+      clashing = clashing or len(set(shapeHashMinus).intersection(set(arenaHash))) != 0 # right
   return clashing
 
-  if direction == '>':
-    leftIndex + len(shape[0]) not in arenaHash # right
-  else:
-    leftIndex - len(shape[0]) not in arenaHash # left
-
-def get_left_index2(direction, leftIndex, shape, arenaLine):
+def get_left_index(direction, leftIndex, shape, arenaLine):
   piecesClash = get_clashing_pieces(direction, leftIndex, shape, arenaLine)
   if direction == '>':
     if leftIndex + len(shape[0]) < 7 and not piecesClash:
@@ -35,14 +28,14 @@ def get_left_index2(direction, leftIndex, shape, arenaLine):
       leftIndex -=1
   return leftIndex
 
-def get_left_index(direction, leftIndex, shape):
-  if direction == '>':
-    if leftIndex + len(shape[0]) < 7:
-      leftIndex +=1
-  else:
-    if leftIndex != 0:
-      leftIndex -=1
-  return leftIndex
+def get_ground_clash(arenaIndex, shape, leftIndex):
+  for l in range(len(shape)):
+    gameHash = [n for n in range(len(arena[arenaIndex+l])) if arena[arenaIndex+l].find('#', n) == n]
+    text = '.......'
+    text = text[:leftIndex] + shape[l] + text[leftIndex+len(shape[l]):]
+    shapeHash = [n for n in range(len(text)) if text.find('#', n) == n]
+    if len(set(gameHash).intersection(set(shapeHash))) != 0:
+      return True
 
 def fall(shape):
   global windIndex, arena
@@ -50,44 +43,39 @@ def fall(shape):
   leftIndex = 2
   settled = False
   arenaIndex = len(arena) - 1
+  arena += ['.......'] * 4 # for shape comparison
 
-  for x in range(0, 1):
-    leftIndex = get_left_index(wind[windIndex], leftIndex, shape)
-    windIndex = (windIndex+1) % 40
-    arenaIndex -= 1
   while not settled:
     #handle windblow
     direction = wind[windIndex]
     windIndex = (1+windIndex)%len(wind)
 
-    leftIndex = get_left_index2(direction, leftIndex, shape, arenaIndex + 1)
+    leftIndex = get_left_index(direction, leftIndex, shape, arenaIndex + 1)
     #handle settle condition
-    gameHash = [n for n in range(len(arena[arenaIndex])-1) if arena[arenaIndex].find('#', n) == n]
-    text = '.......'
-    text = text[:leftIndex] + shape[0] + text[leftIndex+len(shape[0]):]
-    shapeHash = [n for n in range(len(text)-1) if text.find('#', n) == n]
-    if len(set(gameHash).intersection(set(shapeHash))) != 0:
-      for i in range(0,len(shape)):
-        try:
-          text = arena[arenaIndex + 1 + i]
-        except:
-          text = '.......'
-        try:
-          arena[arenaIndex + 1 + i] = (text[:leftIndex] + shape[i] + text[leftIndex+len(shape[i]):])
-        except:
-          arena.append(text[:leftIndex] + shape[i] + text[leftIndex+len(shape[i]):])
-      for j in range(len(arena)-1, len(arena)-4, -1):
+    if get_ground_clash(arenaIndex, shape, leftIndex):
+      for i in range(len(shape)):
+        text = arena[arenaIndex + 1 + i]
+        poses = [(n+leftIndex) for n in range(len(shape[i])) if shape[i].find('#', n) == n]
+        text = list(text)
+        for pos in poses:
+          text[pos] = '#'
+        text = ''.join(text)
+        arena[arenaIndex + 1 + i] = text
+      for j in range(len(arena)-1, len(arena)-11, -1):
         if arena[-1] == '.......':
           del arena[-1]
-      break
+      settled = True
     else:
       arenaIndex -= 1
+f = open("demofile2.txt", "a")
+for rock in range(1000000000000):
+  if windIndex != 0 and rock % (windIndex*len(shapes)) == 0:
+    print(len(arena)-1, rock)
+  fall(shapes[rock%5])
+  f.write(str(len(arena)-1) + '\n')
+f.close()
 
-for rock in range(0,10):
-  fall(shapes[rock%5]) 
-
-  # for i in range(len(arena)-1, -1, -1):
-  #   print(arena[i])
-  # print()
+# for i in range(len(arena)-1, -1, -1):
+#   print(arena[i])
 
 print(len(arena)-1)
